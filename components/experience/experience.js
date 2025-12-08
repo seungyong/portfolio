8c30d28c3e859fc990b5e7dc22a5e41b2a3c958f
seungyong <seungyong20@naver.com>
2025-12-09 02:34:00 +0900

    경험 변경

--- components/experience/experience.js
+++ components/experience/experience.js
@@ -6,17 +6,17 @@ import VerticalTimeline from "../vertical-timeline/verticalTimeline";
 const Experience = () => {
   const timeline = [
     {
-      date: "2025.03 ~ 2026.02",
+      date: "2025.03 - 2026.02",
       title: "서일대학교 전공심화",
       description: "소프트웨어공학과 졸업 예정",
     },
     {
-      date: "2021.03 - 2025.02",
+      date: "2020.03 - 2025.02",
       title: "서일대학교 전문학사",
       description: "소프트웨어공학과 졸업 (학점 : 4.34/4.5)",
     },
     {
-      date: "2020.01 - 2021.01",
+      date: "2019.01 - 2020.01",
       title: "(주)달소프트",
       description: [
         "Vue JS 웹 개발자",
@@ -24,7 +24,7 @@ const Experience = () => {
       ],
     },
     {
-      date: "2017.03 - 2020.02",
+      date: "2017.03 - 2019.02",
       title: "한양공업고등학교",
       description: "컴퓨터 네트워크과 졸업",
     },
